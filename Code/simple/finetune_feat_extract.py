287f5437db80e1664d5fe81e686fd696a8ee40de
K. Prasse <katharina.prasse@posteo.de>
2021-12-11 06:52:40 +0100

    update

--- Code/simple/finetune_feat_extract.py
+++ Code/simple/finetune_feat_extract.py
@@ -13,6 +13,16 @@ data_dir = "/work-ceph/lprasse/siegel/data/siegel_gray_norm"
 batch_size= 200
 device = torch.device("cuda:3")#"cuda:1" or "cpu"
 outname= "fine_tune_batchsize10_epoch50_NR"
+model_name = "inception"
+# Number of classes in the dataset
+num_classes = 4
+# Batch size for training (change depending on how much memory you have)
+batch_size = 5
+# Number of epochs to train for
+num_epochs = 200
+# Flag for feature extracting. When False, we finetune the whole model,
+#   when True we only update the reshaped layer params
+feature_extract = True
 
 ## PREP
 def save_as_pickle(obj, filename):
@@ -38,18 +48,66 @@ def load_pickle(filename):
     with open(filename, 'rb') as file:
         return pickle.load(file)
 
-## Load data - adjust to model
-input_size = 299                                       # required for inception net
+def train_model(model, dataloaders, criterion, optimizer, num_epochs=25, is_inception=False):
+    since = time.time()
 
-data_transforms = transforms.Compose([
-    transforms.Resize((input_size,input_size)),                    
-    #transforms.CenterCrop(224),                 
-    transforms.ToTensor()#,                    
-#     transforms.Normalize(                      
-#     mean = [0.485, 0.456, 0.406],
-#     std = [0.229, 0.224, 0.225]                  
-# )
-])
+    val_acc_history = []
+
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        model.train()  # Set model to training mode
+        
+        # Iterate over data.
+        for inputs, labels in dataloaders[phase]:
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            # forward
+            # track history if only in train
+            with torch.set_grad_enabled(phase == 'train'):
+                # Get model outputs and calculate loss
+                # Special case for inception because in training it has an auxiliary output. In train
+                #   mode we calculate the loss by summing the final output and the auxiliary output
+                #   but in testing we only consider the final output.
+                if is_inception and phase == 'train':
+                    # From https://discuss.pytorch.org/t/how-to-optimize-inception-model-with-auxiliary-classifiers/7958
+                    outputs, aux_outputs = model(inputs)
+                    loss1 = criterion(outputs, labels)
+                    loss2 = criterion(aux_outputs, labels)
+                    loss = loss1 + 0.4*loss2
+                else:
+                    outputs = model(inputs)
+                    loss = criterion(outputs, labels)
+
+                _, preds = torch.max(outputs, 1)
+
+                # backward + optimize only if in training phase
+                if phase == 'train':
+                    loss.backward()
+                    optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / len(dataloaders[phase].dataset)
+            epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+
+    return model
 
 ## Load dataset and create data loader
 image_dataset = datasets.ImageFolder(root=data_dir, transform=data_transforms)
